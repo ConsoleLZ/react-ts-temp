14b646372d4c10ec53644eb3e3be290e03d5a567
liuzhe <1804319025@qq.com>
2025-02-04 01:03:47 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path' //导入Path模块
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; //导入Path模块
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-      // 路径别名
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src') // 配置 @ 别名指向 src 文件夹
-    }
-  }
-})
-
+	plugins: [react()],
+	// 路径别名
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src') // 配置 @ 别名指向 src 文件夹
+		}
+	}
+});
